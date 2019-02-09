35c6ff32a6d8781f62fe08e7e10d38eebd2cf898
Edward Leandro Ríos Duque <edward.duque@tunatraffic.com>
2019-02-09 11:09:39 -0500

    Create Tornado server

--- index.py
+++ index.py
@@ -1,1 +1,20 @@
-print("Hola")
+import os
+
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("<p>Server started</p>")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    port = int(os.environ.get("PORT", 8888))
+    app.listen(port)
+    tornado.ioloop.IOLoop.current().start()
