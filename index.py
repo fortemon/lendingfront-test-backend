8ed9a0a7fbf6960fef8f1a85be384ea437c789f3
Edward Leandro Ríos Duque <edward.duque@tunatraffic.com>
2019-02-09 14:14:55 -0500

    Add headers for CORS

--- index.py
+++ index.py
@@ -5,6 +5,11 @@ import tornado.web
 
 
 class MainHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
     def get(self):
         self.write("<p>Server started</p>")
 
