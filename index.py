39c16670e554c0f6c753fdd398dbb6c9b643da49
Edward Leandro Ríos Duque <edward.duque@tunatraffic.com>
2019-02-09 19:43:34 -0500

    Add handler to receive data for the business loan

--- index.py
+++ index.py
@@ -13,9 +13,31 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("<p>Server started</p>")
 
+class BusinessDataHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
+    def post(self):
+        try:
+            requested_amount = float(self.get_argument('requested_amount', ''))
+        except ValueError:
+            self.set_status(400)
+            self.finish({'message': '`requested_amount` not valid.'})
+            return
+
+        if requested_amount < 50000:
+            self.write({'message': 'Approved'})
+        elif requested_amount > 50000:
+            self.write({'message': 'Declined'})
+        else:
+            self.write({'message': 'Undecided'})
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/businessdata", BusinessDataHandler),
     ])
 
 if __name__ == "__main__":
